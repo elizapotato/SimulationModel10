ecd51ac0c15f360b925f5a97e24f2822bfa599f2
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-06-04 03:35:05 +0700

    Add files via upload

---
+++ Simulation-10/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Simulation_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pbox1 = new System.Windows.Forms.TextBox();
+            this.Pbox2 = new System.Windows.Forms.TextBox();
+            this.Pbox3 = new System.Windows.Forms.TextBox();
+            this.Pbox4 = new System.Windows.Forms.TextBox();
+            this.Pbox5 = new System.Windows.Forms.TextBox();
+            this.Cbox5 = new System.Windows.Forms.TextBox();
+            this.Cbox4 = new System.Windows.Forms.TextBox();
+            this.Cbox3 = new System.Windows.Forms.TextBox();
+            this.Cbox2 = new System.Windows.Forms.TextBox();
+            this.Cbox1 = new System.Windows.Forms.TextBox();
+            this.labWin = new System.Windows.Forms.Label();
+            this.RestartB = new System.Windows.Forms.Button();
+            this.startB = new System.Windows.Forms.Button();
+            this.labOpp = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Pbox1
+            // 
+            this.Pbox1.Enabled = false;
+            this.Pbox1.Location = new System.Drawing.Point(179, 26);
+            this.Pbox1.Name = "Pbox1";
+            this.Pbox1.Size = new System.Drawing.Size(22, 22);
+            this.Pbox1.TabIndex = 1;
+            // 
+            // Pbox2
+            // 
+            this.Pbox2.Enabled = false;
+            this.Pbox2.Location = new System.Drawing.Point(207, 26);
+            this.Pbox2.Name = "Pbox2";
+            this.Pbox2.Size = new System.Drawing.Size(22, 22);
+            this.Pbox2.TabIndex = 2;
+            // 
+            // Pbox3
+            // 
+            this.Pbox3.Enabled = false;
+            this.Pbox3.Location = new System.Drawing.Point(235, 26);
+            this.Pbox3.Name = "Pbox3";
+            this.Pbox3.Size = new System.Drawing.Size(22, 22);
+            this.Pbox3.TabIndex = 3;
+            // 
+            // Pbox4
+            // 
+            this.Pbox4.Enabled = false;
+            this.Pbox4.Location = new System.Drawing.Point(263, 26);
+            this.Pbox4.Name = "Pbox4";
+            this.Pbox4.Size = new System.Drawing.Size(22, 22);
+            this.Pbox4.TabIndex = 4;
+            // 
+            // Pbox5
+            // 
+            this.Pbox5.Enabled = false;
+            this.Pbox5.Location = new System.Drawing.Point(291, 26);
+            this.Pbox5.Name = "Pbox5";
+            this.Pbox5.Size = new System.Drawing.Size(22, 22);
+            this.Pbox5.TabIndex = 5;
+            // 
+            // Cbox5
+            // 
+            this.Cbox5.Enabled = false;
+            this.Cbox5.Location = new System.Drawing.Point(291, 65);
+            this.Cbox5.Name = "Cbox5";
+            this.Cbox5.Size = new System.Drawing.Size(22, 22);
+            this.Cbox5.TabIndex = 10;
+            // 
+            // Cbox4
+            // 
+            this.Cbox4.Enabled = false;
+            this.Cbox4.Location = new System.Drawing.Point(263, 65);
+            this.Cbox4.Name = "Cbox4";
+            this.Cbox4.Size = new System.Drawing.Size(22, 22);
+            this.Cbox4.TabIndex = 9;
+            // 
+            // Cbox3
+            // 
+            this.Cbox3.Enabled = false;
+            this.Cbox3.Location = new System.Drawing.Point(235, 65);
+            this.Cbox3.Name = "Cbox3";
+            this.Cbox3.Size = new System.Drawing.Size(22, 22);
+            this.Cbox3.TabIndex = 8;
+            // 
+            // Cbox2
+            // 
+            this.Cbox2.Enabled = false;
+            this.Cbox2.Location = new System.Drawing.Point(207, 65);
+            this.Cbox2.Name = "Cbox2";
+            this.Cbox2.Size = new System.Drawing.Size(22, 22);
+            this.Cbox2.TabIndex = 7;
+            // 
+            // Cbox1
+            // 
+            this.Cbox1.Enabled = false;
+            this.Cbox1.Location = new System.Drawing.Point(179, 65);
+            this.Cbox1.Name = "Cbox1";
+            this.Cbox1.Size = new System.Drawing.Size(22, 22);
+            this.Cbox1.TabIndex = 6;
+            // 
+            // labWin
+            // 
+            this.labWin.AutoSize = true;
+            this.labWin.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labWin.Location = new System.Drawing.Point(141, 251);
+            this.labWin.Name = "labWin";
+            this.labWin.Size = new System.Drawing.Size(0, 19);
+            this.labWin.TabIndex = 11;
+            // 
+            // RestartB
+            // 
+            this.RestartB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RestartB.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RestartB.Location = new System.Drawing.Point(100, 285);
+            this.RestartB.Name = "RestartB";
+            this.RestartB.Size = new System.Drawing.Size(157, 38);
+            this.RestartB.TabIndex = 12;
+            this.RestartB.Text = "Start Over";
+            this.RestartB.UseVisualStyleBackColor = false;
+            this.RestartB.Click += new System.EventHandler(this.RestartB_Click);
+            // 
+            // startB
+            // 
+            this.startB.BackgroundImage = global::Simulation_10.Properties.Resources.dice_PNG362;
+            this.startB.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startB.Location = new System.Drawing.Point(95, 115);
+            this.startB.Name = "startB";
+            this.startB.Size = new System.Drawing.Size(162, 116);
+            this.startB.TabIndex = 0;
+            this.startB.Text = "START";
+            this.startB.UseVisualStyleBackColor = true;
+            this.startB.Click += new System.EventHandler(this.startB_Click);
+            // 
+            // labOpp
+            // 
+            this.labOpp.AutoSize = true;
+            this.labOpp.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labOpp.Location = new System.Drawing.Point(9, 65);
+            this.labOpp.Name = "labOpp";
+            this.labOpp.Size = new System.Drawing.Size(162, 19);
+            this.labOpp.TabIndex = 13;
+            this.labOpp.Text = "Result of your opponent:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Your result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(352, 341);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labOpp);
+            this.Controls.Add(this.RestartB);
+            this.Controls.Add(this.labWin);
+            this.Controls.Add(this.Cbox5);
+            this.Controls.Add(this.Cbox4);
+            this.Controls.Add(this.Cbox3);
+            this.Controls.Add(this.Cbox2);
+            this.Controls.Add(this.Cbox1);
+            this.Controls.Add(this.Pbox5);
+            this.Controls.Add(this.Pbox4);
+            this.Controls.Add(this.Pbox3);
+            this.Controls.Add(this.Pbox2);
+            this.Controls.Add(this.Pbox1);
+            this.Controls.Add(this.startB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FOUL DICE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startB;
+        private System.Windows.Forms.TextBox Pbox1;
+        private System.Windows.Forms.TextBox Pbox2;
+        private System.Windows.Forms.TextBox Pbox3;
+        private System.Windows.Forms.TextBox Pbox4;
+        private System.Windows.Forms.TextBox Pbox5;
+        private System.Windows.Forms.TextBox Cbox5;
+        private System.Windows.Forms.TextBox Cbox4;
+        private System.Windows.Forms.TextBox Cbox3;
+        private System.Windows.Forms.TextBox Cbox2;
+        private System.Windows.Forms.TextBox Cbox1;
+        private System.Windows.Forms.Label labWin;
+        private System.Windows.Forms.Button RestartB;
+        private System.Windows.Forms.Label labOpp;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Simulation-10/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Simulation_10
+{
+    public partial class Form1 : Form
+    {
+        Game Player;
+        Game Comp;
+        private readonly double[] freqPlayer = { 0.02, 0.03, 0.05, 0.1, 0.3, 0.5 };
+        private readonly double[] freqComp = { 0.5, 0.3, 0.1, 0.05, 0.03, 0.02 };
+        int NumRoll = 5;
+
+        public Form1()
+        {           
+            InitializeComponent();
+            Player = new Game(freqPlayer);
+            Comp = new Game(freqComp);
+            labWin.Text = "";
+        }
+
+        private void startB_Click(object sender, EventArgs e)
+        {            
+            if (NumRoll > 1)
+            for (int i = 0; i < 3; i++)     //Тряска кнопки
+            {
+                startB.Location = new Point(startB.Location.X + 3, startB.Location.Y);
+                Thread.Sleep(100);
+                startB.Location = new Point(startB.Location.X - 3, startB.Location.Y);
+                Thread.Sleep(100);
+            }
+
+            switch (NumRoll)
+            {
+                case 5:
+                    {
+                        Pbox1.Text = Player.Roll() + "";
+                        Cbox1.Text = Comp.Roll() + "";
+                        NumRoll--;
+                        break;
+                    }
+                case 4:
+                    {
+                        Pbox2.Text = Player.Roll() + "";
+                        Cbox2.Text = Comp.Roll() + "";
+                        NumRoll--;
+                        break;
+                    }
+                case 3:
+                    {
+                        Pbox3.Text = Player.Roll() + "";
+                        Cbox3.Text = Comp.Roll() + "";
+                        NumRoll--;
+                        break;
+                    }
+                case 2:
+                    {
+                        Pbox4.Text = Player.Roll() + "";
+                        Cbox4.Text = Comp.Roll() + "";
+                        NumRoll--;
+                        break;
+                    }
+                case 1:
+                    {
+                        Pbox5.Text = Player.Roll() + "";
+                        Cbox5.Text = Comp.Roll() + "";
+                        if (Player.value > Comp.value) labWin.Text = "You are winner!";                        
+                        else
+                        {
+                            if (Player.value < Comp.value) labWin.Text = "You are loser! :(";
+                            else labWin.Text = "Draw!" + Player.value + " ";
+                        }
+                        RestartB.Text = "Start New Game";
+                        break;
+                    }
+                case 0: break;
+            }
+
+        }
+
+        public class Game
+        {
+            private readonly double[] queue;
+            public int value = 0;
+
+            public Game(double[] freq)
+            {
+                queue = freq;
+            }
+
+            public int Roll()
+            {
+                Random random = new Random();
+                int score = 0;
+                var k = random.NextDouble();
+                for (int i = 0; i < queue.Length; i++)
+                {
+                    k -= queue[i];
+                    if (k <= 0)
+                    {
+                        score = i + 1;
+                        value += score;
+                        break;
+                    }
+                    
+                }
+
+                return score;
+            }
+        }
+
+        private void RestartB_Click(object sender, EventArgs e)
+        {
+            RestartB.Text = "Restart";
+            NumRoll = 5;
+            Player.value = Comp.value = 0;
+            Pbox1.Text = Pbox2.Text = Pbox3.Text = Pbox4.Text = Pbox5.Text = "";
+            Cbox1.Text = Cbox2.Text = Cbox3.Text = Cbox4.Text = Cbox5.Text = "";
+            labWin.Text = "";
+
+        }
+    }
+}
